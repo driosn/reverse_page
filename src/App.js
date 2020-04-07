11e9b957ce6282712fc0a9e8cfd899918ed2117a
Magin1104 <magin1104@gmail.com>
2020-04-07 18:12:57 -0400

    homePage finish

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import React,{Fragment}from 'react';
-import HomePage from './components/HomePage'
+import React, {Component} from 'react';
+import {CardList} from './components/card-list/card-list.componets'
+import './App.css';
+import {SearchBox} from './components/search/search-boc.component'
+class App extends Component{
+    constructor(){
+        super();
+        this.state={
+            monsters:[] ,
+            searchField:''            
+        };
+    }
 
-function App() {
-  return (
-    <Fragment>
-        <HomePage/>
-    </Fragment>
-  );
+    componentDidMount(){           
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=>response.json())
+        .then(user => this.setState({monsters:user})) 
+    }
+    render(){
+        const { monsters, searchField } = this.state;
+        const filteredMonsters = monsters.filter(monster =>
+                monster.name.toLowerCase().includes(searchField.toLowerCase())
+            )        
+        return(
+            <div className="App">
+            <h1> Monsters Rolodex </h1>
+            <SearchBox
+                placeholder='Search Monsters'
+                handleChange={e =>this.setState({searchField: e.target.value})}
+            />
+            <CardList monsters={filteredMonsters}/>
+          </div>
+        );
+    }
 }
+
 export default App;
