a775e127bead98ace94fca24bdff8f688f40af66
David Rios <davidsamuelrios02@gmail.com>
2020-05-03 19:35:57 -0400

    Refactored positions in create challenge page

--- src/pages/UploadChallengePage/components/createChallenge.component.jsx
+++ src/pages/UploadChallengePage/components/createChallenge.component.jsx
@@ -29,69 +29,71 @@ class CreateChallenge extends React.Component{
 
     render(){
         return(
-            <div className= 'newch' >
-            <form onSubmit={this.handleSubmit}>
-                <div className="pos">
-                <label className="sel">Seleccionar Categoria</label>
-                <select class="form-control">
-                    <option>Criptografia</option>
-                    <option>Esteganografia</option>
-                    <option>Trafico de Datos</option>
-                    <option>Sql Injection</option>
-                </select>
-                </div>   
-                <div className="pos"></div>
-                <br/>
-                <div className="pos1">
-                <label className="sel">Titulo:</label>
-                <input 
-                    className="form-control"
-                    name = "title" 
-                    placeholder="Escribe el titulo de tu reto"
-                    value={this.state.email}
-                    onChange={this.handleChange}
-                    required
-                /><br/>
-                <label className="sel">Descripcion:</label>
-                <textarea 
-                    className="form-control"
-                    placeholder="Escribe el enunciado de tu reto"
-                    name = "description" 
-                    rows="4"
-                    value={this.state.description}
-                    onChange={this.handleChange}
-                    required
-                />
-                <br/>
+            <div className="new-challenge-container">
+                <div className= 'newch mt-5' >
+                    <form onSubmit={this.handleSubmit}>
+                        <div className="pos">
+                        <label className="sel">Seleccionar Categoria</label>
+                        <select class="form-control">
+                            <option>Criptografia</option>
+                            <option>Esteganografia</option>
+                            <option>Trafico de Datos</option>
+                            <option>Sql Injection</option>
+                        </select>
+                        </div>   
+                        <div className="pos"></div>
+                        <br/>
+                        <div className="pos1">
+                        <label className="sel">Titulo:</label>
+                        <input 
+                            className="form-control"
+                            name = "title" 
+                            placeholder="Escribe el titulo de tu reto"
+                            value={this.state.email}
+                            onChange={this.handleChange}
+                            required
+                        /><br/>
+                        <label className="sel">Descripcion:</label>
+                        <textarea 
+                            className="form-control"
+                            placeholder="Escribe el enunciado de tu reto"
+                            name = "description" 
+                            rows="4"
+                            value={this.state.description}
+                            onChange={this.handleChange}
+                            required
+                        />
+                        <br/>
+                        </div>
+                        <div className="pos1">
+                        <label className="sel">Flag / Respuesta </label>
+                        <input
+                            className="form-control"
+                            placeholder="Escribe la Flag correcta"
+                            name = "flag" 
+                            value={this.state.flag}
+                            onChange={this.handleChange}
+                            required
+                        />
+                        <br/>
+                        <label className="sel">Confirmar Flag</label>
+                        <input 
+                            className="form-control"
+                            placeholder="Confirmar Flag"
+                            name = "confirmflag" 
+                            value={this.state.cofirmflag}
+                            onChange={this.handleChange}
+                            required    
+                        />
+                        <br/>
+                        <input 
+                            className="btn btn-primary"
+                            type='submit'
+                            value = 'Enviar'
+                        />
+                        </div>
+                    </form>
                 </div>
-                <div className="pos1">
-                <label className="sel">Flag / Respuesta </label>
-                <input
-                    className="form-control"
-                    placeholder="Escribe la Flag correcta"
-                    name = "flag" 
-                    value={this.state.flag}
-                    onChange={this.handleChange}
-                    required
-                />
-                <br/>
-                <label className="sel">Confirmar Flag</label>
-                <input 
-                    className="form-control"
-                    placeholder="Confirmar Flag"
-                    name = "confirmflag" 
-                    value={this.state.cofirmflag}
-                    onChange={this.handleChange}
-                    required    
-                />
-                <br/>
-                <input 
-                    className="btn btn-primary"
-                    type='submit'
-                    value = 'Enviar'
-                />
-                </div>
-            </form>
             </div>
         )
     }
